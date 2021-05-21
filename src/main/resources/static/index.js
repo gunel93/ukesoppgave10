b3624667d522087f76dbf2ef311143e0850b0ee6
gunel93 <s339490@oslomet.no>
2021-05-21 19:14:06 +0200

    Nye filer ble lagt til

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,65 +1,6 @@
 $(function(){  // kjøres når dokumentet er ferdig lastet
-    hentAlleBiler();
     hentAlle();
 });
-
-function hentAlleBiler() {
-    $.get( "/hentBiler", function( biler ) {
-        formaterBiler(biler);
-    });
-}
-
-function formaterBiler(biler){
-    let ut = "<select id='valgtMerke' onchange='finnTyper()'>";
-    let forrigeMerke = "";
-    ut+="<option>Velg merke</option>";
-    for (const bil of biler){
-        if(bil.merke !== forrigeMerke){
-            ut+="<option>"+bil.merke+"</option>";
-        }
-        forrigeMerke = bil.merke;
-    }
-    ut+="</select>";
-    $("#merke").html(ut);
-}
-
-function finnTyper(){
-    const valgtMerke = $("#valgtMerke").val();
-    $.get( "/hentBiler", function( biler ) {
-        formaterTyper(biler,valgtMerke);
-    });
-}
-function formaterTyper(biler,valgtMerke){
-    let ut = "<select id='valgtType'>";
-    for(const bil of biler ){
-        if(bil.merke === valgtMerke){
-            ut+="<option>"+bil.type+"</option>";
-        }
-    }
-    ut+="</select>";
-    $("#type").html(ut);
-}
-
-function regMotorvogn() {
-    const motorvogn = {
-        personnr : $("#personnr").val(),
-        navn : $("#navn").val(),
-        adresse : $("#adresse").val(),
-        kjennetegn : $("#kjennetegn").val(),
-        merke : $("#valgtMerke").val(),
-        type : $("#valgtType").val(),
-    };
-    $.post("/lagre", motorvogn, function(){
-        hentAlle();
-    });
-    $("#personnr").val("");
-    $("#navn").val("");
-    $("#adresse").val("");
-    $("#kjennetegn").val("");
-    $("#valgtMerke").val("");
-    $("#valgtType").val("");
-}
-
 function hentAlle() {
     $.get( "/hentAlle", function( biler ) {
         formaterData(biler);
@@ -72,11 +13,17 @@ function formaterData(biler) {
     for (const bil of biler) {
         ut += "<tr><td>" + bil.personnr + "</td><td>" + bil.navn + "</td><td>" + bil.adresse + "</td>" +
             "<td>" + bil.kjennetegn + "</td><td>" + bil.merke + "</td><td>" + bil.type + "</td><" +
+            "td><button class='btn btn-primary' onclick='idTilEndring("+ bil.id + ")'>Endre</button> </td>" +
             "td><button class='btn btn-danger' onclick='slettEnMotorvogn("+ bil.personnr + ")'>Slett</button> </td>" +
             "</tr>";
     }
     ut += "</table>";
     $("#bilene").html(ut);
+}
+
+function idTilEndring(id) {
+    window.location.href = "/endring.html?" + id;
+
 }
 function slettEnMotorvogn(personnr) {
     const url = "/slettEnMotorvogn?personnr=" + personnr;
